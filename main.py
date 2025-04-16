9e4f903184a76ce0158e8bf63463bbde21303d8a
SPARSH BATRA <sbatrabusiness@gmail.com>
2025-04-16 23:49:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from data_loading import load_data
 from data_cleaning import clean_data
 from data_preprocessing import preprocess_data
 from eda import perform_eda
-from xgboost_model import train_xgboost_model  # Updated import
+from xgboost_model import train_xgboost_model
+from model_evaluation import evaluate_model  # NEW IMPORT
 
 # Define your CSV file path
-file_path = 'atp_matches_2024.csv'  # Replace with your actual file path
+file_path = 'atp_matches_2024.csv'
 
-# Load the data once
+# Load the data
 df = load_data(file_path)
 
 # Clean the data
@@ -19,7 +20,11 @@ df_preprocessed = preprocess_data(df_cleaned)
 # Perform EDA
 perform_eda(df_preprocessed)
 
-# Train and evaluate the Random Forest model
+# Train XGBoost model
 print("\nTraining and Evaluating XGBOOST Model...")
-model, X_test, y_test, y_pred = train_xgboost_model(df_preprocessed)
+xgb_model, X_train, X_test, y_train, y_test, y_pred = train_xgboost_model(df_preprocessed)
 print("XGBOOST Model training completed.")
+
+# Evaluate model performance
+print("\nEvaluating Model Performance...")
+evaluate_model(xgb_model, X_train, X_test, y_train, y_test)
